937725209909c2ef40f7b6ca6e9879276752e728
miyossy75-alt <miyossy75@gmail.com>
2026-04-21 03:05:02 +0900

    Update cache version and assets list in sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'receipt-scanner-v1';
-const ASSETS = ['./','./index.html','./manifest.json'];
+const CACHE = 'receipt-scanner-v2';
+const ASSETS = ['./', './index.html', './manifest.json'];
 self.addEventListener('install', e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS))));
 self.addEventListener('fetch', e => e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))));
