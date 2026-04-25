9d2c41df692e9f41dae169536486ed143bfee470
miyossy75-alt <miyossy75@gmail.com>
2026-04-26 01:47:13 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,14 +1,13 @@
-const CACHE = 'receipt-scanner-v2';
-const ASSETS = ['./', './index.html', './manifest.json'];
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
-  self.skipWaiting();
-});
+const CACHE = 'receipt-scanner-v3';
+self.addEventListener('install', e => { self.skipWaiting(); });
 self.addEventListener('activate', e => {
   e.waitUntil(caches.keys().then(keys =>
-    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    Promise.all(keys.map(k => caches.delete(k)))
   ));
+  self.clients.claim();
 });
 self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
+  );
 });
